fc741bf56340bca352df0b34b2d81a3e9505a234
edgeboyo <mkolybko@gmail.com>
2020-11-06 16:18:59 +0000

    Better setup script

--- setup.sh
+++ setup.sh
@@ -1,7 +1,9 @@
 sudo apt update
 sudo apt install apache2 -y
 cd /tmp/
+rm -rf controlDash
+sudo rm -rf /var/www/html/*
 git clone https://github.com/edgeboyo/controlDash
-sudo mv controlDash/* /var/www/html/
+sudo mv controlDash/* /var/www/html/ -f
 
 sudo sh -c "echo 'www-data     ALL=(ALL) NOPASSWD:ALL' >> /etc/sudoers"
