5c1b4f99521a316b695c81f866f734b853c6c851
edgeboyo <45755397+edgeboyo@users.noreply.github.com>
2020-11-06 03:20:31 +0000

    Added root control for moving

--- setup.sh
+++ setup.sh
@@ -2,6 +2,6 @@ sudo apt update
 sudo apt install apache2 -y
 cd /tmp/
 git clone https://github.com/edgeboyo/controlDash
-mv controlDash/* /var/www/html/
+sudo mv controlDash/* /var/www/html/
 
 #add www-date to sudo'ers
