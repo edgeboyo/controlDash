55f9e11c2e8c7544351828f1d31b42b73e7aaefc
edgeboyo <45755397+edgeboyo@users.noreply.github.com>
2020-11-07 14:19:22 +0000

    New password input

--- changeUsrPass.php
+++ changeUsrPass.php
@@ -11,7 +11,7 @@
 <h2>Log user off</h2>
     <form action="/change.php" method="post">
     <label>
-        Select user to expire the password:</label><br/>
+        Select user to expire the password:</label>
     <select name="user">
     	<?php
 	$list = shell_exec("cat /etc/passwd | grep nologin -v | awk -F':' '{ print $1}'");
@@ -19,11 +19,13 @@
 	$arr = explode("\n", $list);
 
 	foreach($arr as $usr){
-    if($usr != "root")
+    		if($usr != "root")
 		  echo "<option value=" . $usr . ">" . $usr . "</option>";
 	}
 	?>
 	</select><br/>
+	<label> Enter password: </label>
+	<input type="password" name ="password" required/> </br>
     <input type="submit" value="Log user out..."/>
 
 </form>
